4b151cf72a58dcf0d260909dcbbbd98988697153
Marko Sustarsic <marchee88@gmail.com>
2016-04-06 17:28:18 +0200

    Update 12-6kyu-build-a-pile-of-cubes.js

--- 12-6kyu-build-a-pile-of-cubes.js
+++ 12-6kyu-build-a-pile-of-cubes.js
@@ -16,26 +16,26 @@ Try to optimize your function.
 */
 
 function findNb(m) {
-    var sum = 0;
-    var i = 1;
-    while (sum <m) {
-    	sum = sum + Math.pow(i, 3);
-    	i++;
-    }
-    if (sum === m) {
-    	return (i-1);
-    } else {
-    	return -1;
-    }
+  var sum = 0;
+  var i = 1;
+  while (sum <m) {
+    sum = sum + Math.pow(i, 3);
+    i++;
+  }
+  if (sum === m) {
+    return (i-1);
+  } else {
+    return -1;
+  }
 }
 
 // Best solution (tidied)
 
 function findNb(m) {
-	var n = 0;
-	while (m > 0) {
-  		n=n+1;
-  		m=m-Math.pow(n,3); 
-  	}
-	return m ? -1 : n;
+  var n = 0;
+  while (m > 0) {
+    n=n+1;
+    m=m-Math.pow(n,3); 
+  }
+  return m ? -1 : n;
 }
